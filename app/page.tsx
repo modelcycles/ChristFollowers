69a89caef0378a916ed71f5e33c324b10625dfb1
modelcycles <whdydcjf0561@gmail.com>
2024-05-01 13:49:02 +0900

    feat: added images

--- app/page.tsx
+++ app/page.tsx
@@ -109,12 +109,12 @@ function Header() {
         height: C.header_height,
         background: C.color_sonic_blue,
         justifyContent: "space-between",
+        alignItems: "center",
         display: "flex",
       }}
     >
       <div
         style={{
-          padding: 35,
           fontFamily: "MonoFont, monospace",
           fontSize: 20,
           // letterSpacing: "0.01em",
@@ -129,7 +129,6 @@ function Header() {
       >
         <div
           style={{
-            marginTop: 38,
             marginRight: 38,
             fontFamily: "MonoFont, monospace",
           }}
@@ -138,7 +137,6 @@ function Header() {
         </div>
         <div
           style={{
-            marginTop: 38,
             marginRight: 38,
             fontFamily: "MonoFont, monospace",
           }}
@@ -153,7 +151,6 @@ function Header() {
       >
         <div
           style={{
-            paddingTop: 38,
             marginRight: 20,
           }}
         >
@@ -161,7 +158,6 @@ function Header() {
         </div>
         <div
           style={{
-            paddingTop: 38,
             marginRight: 20,
           }}
         >
@@ -193,83 +189,90 @@ function ImageLoader() {
       <div
         style={{
           display: "grid",
+          alignContent: "start",
+          transition: "0.3s",
         }}
       >
-        <img src={"testPhoto/6.jpeg"} width={"100%"} />
-        <img src={"testPhoto/5.jpeg"} width={"100%"} />
-        <img src={"testPhoto/3.jpeg"} width={"100%"} />
-        <img src={"testPhoto/2.jpeg"} width={"100%"} />
         <img src={"testPhoto/0.jpeg"} width={"100%"} />
-        <img src={"testPhoto/8.jpeg"} width={"100%"} />
-        <img src={"testPhoto/12.jpeg"} width={"100%"} />
-        <img src={"testPhoto/7.jpeg"} width={"100%"} />
-        <img src={"testPhoto/17.jpeg"} width={"100%"} />
-        <img src={"testPhoto/4.jpeg"} width={"100%"} />
-        <img src={"testPhoto/13.jpeg"} width={"100%"} />
-        <img src={"testPhoto/1.jpeg"} width={"100%"} />
-        <img src={"testPhoto/14.jpeg"} width={"100%"} />
-        <img src={"testPhoto/11.jpeg"} width={"100%"} />
+        <img src={"testPhoto/3.jpeg"} width={"100%"} />
+        <img src={"testPhoto/6.jpeg"} width={"100%"} />
         <img src={"testPhoto/9.jpeg"} width={"100%"} />
-        <img src={"testPhoto/20.jpeg"} width={"100%"} />
-        <img src={"testPhoto/16.jpeg"} width={"100%"} />
-        <img src={"testPhoto/10.jpeg"} width={"100%"} />
-        <img src={"testPhoto/19.jpeg"} width={"100%"} />
+        <img src={"testPhoto/12.jpeg"} width={"100%"} />
         <img src={"testPhoto/15.jpeg"} width={"100%"} />
         <img src={"testPhoto/18.jpeg"} width={"100%"} />
+        <img src={"testPhoto/21.jpg"} width={"100%"} />
+        <img src={"testPhoto/24.jpg"} width={"100%"} />
+        <img src={"testPhoto/27.jpg"} width={"100%"} />
+        <img src={"testPhoto/30.jpg"} width={"100%"} />
+        <img src={"testPhoto/33.jpg"} width={"100%"} />
+        <img src={"testPhoto/36.jpg"} width={"100%"} />
+        <img src={"testPhoto/39.jpg"} width={"100%"} />
+        <img src={"testPhoto/42.jpg"} width={"100%"} />
+        <img src={"testPhoto/45.jpg"} width={"100%"} />
+        <img src={"testPhoto/48.jpg"} width={"100%"} />
+        <img src={"testPhoto/51.jpg"} width={"100%"} />
+        <img src={"testPhoto/54.jpg"} width={"100%"} />
+        <img src={"testPhoto/57.jpg"} width={"100%"} />
+        <img src={"testPhoto/60.jpg"} width={"100%"} />
       </div>
       <div
         style={{
           display: "grid",
+          alignContent: "start",
+          transition: "0.3s",
         }}
       >
-        <img src={"testPhoto/4.jpeg"} width={"100%"} />
-        <img src={"testPhoto/0.jpeg"} width={"100%"} />
-        <img src={"testPhoto/15.jpeg"} width={"100%"} />
-        <img src={"testPhoto/2.jpeg"} width={"100%"} />
         <img src={"testPhoto/1.jpeg"} width={"100%"} />
-        <img src={"testPhoto/9.jpeg"} width={"100%"} />
-        <img src={"testPhoto/6.jpeg"} width={"100%"} />
-        <img src={"testPhoto/3.jpeg"} width={"100%"} />
-        <img src={"testPhoto/16.jpeg"} width={"100%"} />
-        <img src={"testPhoto/17.jpeg"} width={"100%"} />
-        <img src={"testPhoto/11.jpeg"} width={"100%"} />
-        <img src={"testPhoto/8.jpeg"} width={"100%"} />
         <img src={"testPhoto/5.jpeg"} width={"100%"} />
-        <img src={"testPhoto/13.jpeg"} width={"100%"} />
-        <img src={"testPhoto/7.jpeg"} width={"100%"} />
-        <img src={"testPhoto/18.jpeg"} width={"100%"} />
-        <img src={"testPhoto/19.jpeg"} width={"100%"} />
-        <img src={"testPhoto/12.jpeg"} width={"100%"} />
-        <img src={"testPhoto/14.jpeg"} width={"100%"} />
-        <img src={"testPhoto/20.jpeg"} width={"100%"} />
+        <img src={"testPhoto/8.jpeg"} width={"100%"} />
         <img src={"testPhoto/10.jpeg"} width={"100%"} />
+        <img src={"testPhoto/13.jpeg"} width={"100%"} />
+        <img src={"testPhoto/16.jpeg"} width={"100%"} />
+        <img src={"testPhoto/19.jpeg"} width={"100%"} />
+        <img src={"testPhoto/22.jpg"} width={"100%"} />
+        <img src={"testPhoto/25.jpg"} width={"100%"} />
+        <img src={"testPhoto/28.jpg"} width={"100%"} />
+        <img src={"testPhoto/31.jpg"} width={"100%"} />
+        <img src={"testPhoto/34.jpg"} width={"100%"} />
+        <img src={"testPhoto/37.jpg"} width={"100%"} />
+        <img src={"testPhoto/40.jpg"} width={"100%"} />
+        <img src={"testPhoto/43.jpg"} width={"100%"} />
+        <img src={"testPhoto/46.jpg"} width={"100%"} />
+        <img src={"testPhoto/49.jpg"} width={"100%"} />
+        <img src={"testPhoto/52.jpg"} width={"100%"} />
+        <img src={"testPhoto/55.jpg"} width={"100%"} />
+        <img src={"testPhoto/58.jpg"} width={"100%"} />
+        <img src={"testPhoto/63.jpg"} width={"100%"} />
       </div>
       <div
         style={{
           display: "grid",
+          alignContent: "start",
+          transition: "0.3s",
         }}
       >
-        <img src={"testPhoto/5.jpeg"} width={"100%"} />
-        <img src={"testPhoto/8.jpeg"} width={"100%"} />
-        <img src={"testPhoto/7.jpeg"} width={"100%"} />
-        <img src={"testPhoto/13.jpeg"} width={"100%"} />
-        <img src={"testPhoto/6.jpeg"} width={"100%"} />
-        <img src={"testPhoto/16.jpeg"} width={"100%"} />
-        <img src={"testPhoto/1.jpeg"} width={"100%"} />
-        <img src={"testPhoto/4.jpeg"} width={"100%"} />
-        <img src={"testPhoto/18.jpeg"} width={"100%"} />
         <img src={"testPhoto/2.jpeg"} width={"100%"} />
-        <img src={"testPhoto/0.jpeg"} width={"100%"} />
-        <img src={"testPhoto/9.jpeg"} width={"100%"} />
-        <img src={"testPhoto/10.jpeg"} width={"100%"} />
-        <img src={"testPhoto/15.jpeg"} width={"100%"} />
-        <img src={"testPhoto/14.jpeg"} width={"100%"} />
-        <img src={"testPhoto/19.jpeg"} width={"100%"} />
-        <img src={"testPhoto/20.jpeg"} width={"100%"} />
-        <img src={"testPhoto/3.jpeg"} width={"100%"} />
+        <img src={"testPhoto/4.jpeg"} width={"100%"} />
+        <img src={"testPhoto/7.jpeg"} width={"100%"} />
         <img src={"testPhoto/11.jpeg"} width={"100%"} />
+        <img src={"testPhoto/14.jpeg"} width={"100%"} />
         <img src={"testPhoto/17.jpeg"} width={"100%"} />
-        <img src={"testPhoto/12.jpeg"} width={"100%"} />
+        <img src={"testPhoto/20.jpeg"} width={"100%"} />
+        <img src={"testPhoto/23.jpg"} width={"100%"} />
+        <img src={"testPhoto/26.jpg"} width={"100%"} />
+        <img src={"testPhoto/29.jpg"} width={"100%"} />
+        <img src={"testPhoto/32.jpg"} width={"100%"} />
+        <img src={"testPhoto/35.jpg"} width={"100%"} />
+        <img src={"testPhoto/38.jpg"} width={"100%"} />
+        <img src={"testPhoto/41.jpg"} width={"100%"} />
+        <img src={"testPhoto/44.jpg"} width={"100%"} />
+        <img src={"testPhoto/47.jpg"} width={"100%"} />
+        <img src={"testPhoto/50.jpg"} width={"100%"} />
+        <img src={"testPhoto/53.jpg"} width={"100%"} />
+        <img src={"testPhoto/56.jpg"} width={"100%"} />
+        <img src={"testPhoto/59.jpg"} width={"100%"} />
+        <img src={"testPhoto/62.jpg"} width={"100%"} />
+        <img src={"testPhoto/61.jpg"} width={"100%"} />
       </div>
     </>
   );
